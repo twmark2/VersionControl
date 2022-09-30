b59739030fb79d55f1ce01c8200d54c7436907e4
Márk Tóth-Wessely <mark.toth-wessely@stud.uni-corvinus.hu>
2022-09-30 12:10:57 +0200

    Resource+Form changes

---
+++ UserMaintenance/UserMaintenance/Entities/Ember.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+    public class Ember
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        private string FullName;
+
+        public string MyProperty
+        {
+            get 
+            {
+                return string.Format("{0}{1}", LastName, FirstName);
+            }
+           
+        }
+
+
+
+    }
+}
