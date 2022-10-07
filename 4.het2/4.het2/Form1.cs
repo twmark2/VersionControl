729d3e27e91869a0c4c1ef4a9ea7995ad794b8c1
Márk Tóth-Wessely <mark.toth-wessely@stud.uni-corvinus.hu>
2022-10-07 11:59:57 +0200

    4.f

--- 4.het2/4.het2/Form1.cs
+++ 4.het2/4.het2/Form1.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _4.het2
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
+            Loaddata();
         }
-    }
+        private void Loaddata()
+        {
+
+            Flats = context.Flats.ToList();
+        }
+
+}
 }
