f65aef231ef65195ea9d87e7c7e2fdc48046c7df
Márk Tóth-Wessely <mark.toth-wessely@stud.uni-corvinus.hu>
2022-10-21 12:08:54 +0200

    4/1

--- UserMaintenance/WindowsFormsApp1/Form1.cs
+++ UserMaintenance/WindowsFormsApp1/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Entities;
+using WindowsFormsApp1.MnbServiceReference;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form       
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void asd() 
+        {
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+
         }
     }
 }
