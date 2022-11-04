9821cb8eeb727164060d90c1734790b358e304ca
Márk Tóth-Wessely <mark.toth-wessely@stud.uni-corvinus.hu>
2022-11-04 13:04:53 +0100

    7.commit

--- 6.het/6.het/6.het/Form1.cs
+++ 6.het/6.het/6.het/Form1.cs
@@ -1,4 +1,5 @@
-﻿using _6.het.Entities;
+﻿using _6.het.Abstractions;
+using _6.het.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,9 +14,9 @@ namespace _6.het
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
+        private List<Toy> _toys = new List<Toy>();
         private BallFactory _factory;
-        public BallFactory Factory
+        public BallFactory IToyFactory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -24,21 +25,21 @@ namespace _6.het
         public Form1()
         {
             InitializeComponent();
-            Factory = new BallFactory();
+            IToyFactory = new BallFactory();
 
         }
 
         private void CreateTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
+            var ball = IToyFactory.CreateNew();
+            _toys.Add(ball);
             ball.Left = -ball.Width;
             MainPanel.Controls.Add(ball);
         }
 
         private void ConveyorTimer_Tick(object sender, EventArgs e)
         {
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
                 var maxPosition = 0;
                 ball.MoveToy();
@@ -51,9 +52,9 @@ namespace _6.het
                     }
                     if (maxPosition>1000)
                     {
-                        var oldestBall = _balls[0];
+                        var oldestBall = _toys[0];
                         MainPanel.Controls.Remove(oldestBall);
-                        _balls.Remove(oldestBall);
+                        _toys.Remove(oldestBall);
                     }
                 }
             }
