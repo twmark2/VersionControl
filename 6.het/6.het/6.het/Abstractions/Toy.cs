7ee282842fecb0b87522626cb9c43e0f0ed85e73
Márk Tóth-Wessely <mark.toth-wessely@stud.uni-corvinus.hu>
2022-11-04 12:42:53 +0100

    4.commit

---
+++ 6.het/6.het/6.het/Abstractions/Toy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6.het.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics g);
+
+        public void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
