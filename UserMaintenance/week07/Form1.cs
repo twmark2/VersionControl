6b4197baecbcb4a9a47bc9a4d01989942842fcd4
Márk Tóth-Wessely <mark.toth-wessely@stud.uni-corvinus.hu>
2022-11-11 12:16:41 +0100

    2.

--- UserMaintenance/week07/Form1.cs
+++ UserMaintenance/week07/Form1.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week07.Entities;
 
 namespace week07
 {
     public partial class Form1 : Form
     {
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        PortfolioEntities context = new PortfolioEntities();    
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+
         }
+        private void CreatePortfolio() 
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+        
     }
 }
