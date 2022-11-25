f84974375c7056d03224de3d0df8931bb9ebda63
Márk Tóth-Wessely <mark.toth-wessely@stud.uni-corvinus.hu>
2022-11-25 12:28:51 +0100

    2nd

---
+++ UserMaintenance/UnitTestExample.Test3/AccountControllerTestFixture.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test3
+{
+    public class AccountControllerTestFixture
+    {
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+           
+            var actualResult = accountController.ValidateEmail(email);
+
+            
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
