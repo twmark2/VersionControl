ad58eb5ef2bcc7f3e3d84631f376e7b6ecbfd1aa
Márk Tóth-Wessely <mark.toth-wessely@stud.uni-corvinus.hu>
2022-11-25 12:42:45 +0100

    6th

--- UserMaintenance/UnitTestExample.Test3/AccountControllerTestFixture.cs
+++ UserMaintenance/UnitTestExample.Test3/AccountControllerTestFixture.cs
@@ -1,6 +1,9 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
+using UnitTestExample.Abstractions;
 using UnitTestExample.Controllers;
+using UnitTestExample.Entities;
 
 namespace UnitTestExample.Test3
 {
@@ -75,6 +78,32 @@ namespace UnitTestExample.Test3
             
         }
 
+        [
+    Test,
+    TestCase("irf@uni-corvinus.hu", "Abcd1234")
+]
+        public void TestRegisterApplicationException(string newEmail, string newPassword)
+        {
+            
+            var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+            accountServiceMock
+                .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+                .Throws<ApplicationException>();
+            var accountController = new AccountController();
+            accountController.AccountManager = accountServiceMock.Object;
 
+            
+            try
+            {
+                var actualResult = accountController.Register(newEmail, newPassword);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ApplicationException>(ex);
+            }
+
+           
+        }
     }
 }
