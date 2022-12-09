7e7eba23293c647f50dc19ce1d0524fd29543e26
Márk Tóth-Wessely <mark.toth-wessely@stud.uni-corvinus.hu>
2022-12-09 12:23:05 +0100

    4th

--- UserMaintenance/week10/week10/Form1.cs
+++ UserMaintenance/week10/week10/Form1.cs
@@ -43,6 +43,33 @@ namespace week10
         {
             generation++;
             label1.Text = string.Format("{0}. generáció", generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+
         }
+
+
+
+
     }
 }
